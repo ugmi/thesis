54801ce84b61ac4da335300ce8afc5b2407ff218
ugnemi <ugnemi@kth.se>
2026-01-31 11:51:30 +0100

    Adjust plots for legibility

--- data_generation_v3.R
+++ data_generation_v3.R
@@ -35,17 +35,17 @@ plot(o, 1 - pweibull(o, 0.8, 500), xlab = "Days", ylab = "Survival probability",
 df <- data.frame("true" = round(t, 5), 
                  "days" = floor(t),
                  "lost.flw" = rbinom(N, 1, 1/14),
-                 "t.entry" = floor(runif(N, 1, 1095)))
+                 "entry" = floor(runif(N, 1, 1095)))
 df["status"] <- ifelse(df$lost.flw, "Lost to follow up",
-                       ifelse(df$t.entry + df$days > 1095, "Alive", "Dead"))
-df["t.exit"] <- ifelse(df$status == "Alive", 1095,
-                       df$t.entry + ifelse(!df$lost.flw, df$days, 
-                                            floor(pmin(1095 - df$t.entry, df$days)*runif(N))))
-df.obs <- df[c("status", "t.entry", "t.exit")]
+                       ifelse(df$entry + df$days > 1095, "Alive", "Dead"))
+df["exit"] <- ifelse(df$status == "Alive", 1095,
+                     df$entry + ifelse(!df$lost.flw, df$days, 
+                                       floor(pmin(1095 - df$entry, df$days)*runif(N))))
+df.obs <- df[c("status", "entry", "exit")]
 
 # Modelling using proposed approach --------------------------------------------
 # Define coarsening limits
-l <- df.obs$t.exit - df.obs$t.entry
+l <- df.obs$exit - df.obs$entry
 u <- ifelse(df.obs$status == "Dead", l + 1, Inf)
 
 # Helper functions
@@ -67,25 +67,25 @@ mle.gamma <- nlm(logL, c(1, 1/100), l, u, hessian = TRUE)
 
 # Plot and compare the distributions
 plot(o, 1 - pweibull(o, 0.8, 500), pch=20, main = "Fitted survival curves",
-     xlab = "Days", ylab = "Survival probability")
-points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#FF00A033")
+     xlab = "Days", ylab = "Survival probability", cex=0.1)
+points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#DF99FF66", cex=0.1)
 points(o, 1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]), 
-       pch = 20, col = "#80DF3033")
+       pch = 20, col = "#72BF4066", cex=0.1)
 points(o, 1 - plogis(o, mle.logis$estimate[1], mle.logis$estimate[2]), 
-       pch = 20, col = "#3060FF33")
+       pch = 20, col = "#3060FF66", cex=0.1)
 points(o, 1 - pgamma(o, mle.gamma$estimate[1], mle.gamma$estimate[2]),
-       pch = 20, col = "#F5FF8333")
+       pch = 20, col = "#FF000066", cex=0.1)
 legend("topright", lty = rep(1, 5), bty = "n",
-       col = c("black", "#FF00A099", "#80DF3099", "#3060FF99", "#F5FF8399"), 
+       col = c("black", "#DF99FF99", "#72BF4099", "#3060FF99", "#FF000099"), 
        legend = c("True distribution", "Exponential", "Weibull", "Logistic", "Gamma"))
 
 # From the plot, we see that Weibull and Gamma distributions are close to the truth.
 
 # Plot the Weibull distribution only
 plot(o, 1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]),
-     pch = 20, col = "red",  main = "Weibull survival curves",
+     pch = 20, col = "red",  cex=0.1, main = "Weibull survival curves",
      xlab = "Days", ylab = "Survival probability")
-points(o, 1 - pweibull(o, 0.8, 500), pch = 20)
+points(o, 1 - pweibull(o, 0.8, 500), pch = 20, cex=0.1)
 legend("topright", lty = c(1, 1), bty = "n", col = c("black", "red"),
        legend = c("True distribution", "Fitted distribution"))
 
@@ -94,7 +94,24 @@ c(0.8, 500) - mle.weibull$estimate
 
 # Conventional approaches ------------------------------------------------------
 
+plot(survfit(Surv(exit - entry, status == "Dead") ~ 1, data = df.obs, 
+             conf.type = "log-log"), 
+     main = "Kaplan-Meier survival curve", xlab = "Days", ylab = "Survival",
+     col = "red", xlim = c(0, 5000))
+points(o, 1 - pweibull(o, 0.8, 500), pch = 20, col="black", cex=0.1)
+#lines(survfit(coxph(Surv(exit - entry, status == "Dead") ~ 1, data = df.obs)), col="blue")
+# No need to add Cox model since we have no covariates
 
-
-
+# Compare with some of the fitted models from before
+plot(survfit(Surv(exit - entry, status == "Dead") ~ 1, data = df.obs, 
+             conf.type = "log-log"),
+     main = "Kaplan-Meier vs proposed approach", xlab = "Days", ylab = "Survival")
+points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#DF99FF66", cex=0.1)
+points(o, 1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]), 
+       pch = 20, col = "#72BF4066", cex=0.1)
+points(o, 1 - pgamma(o, mle.gamma$estimate[1], mle.gamma$estimate[2]),
+       pch = 20, col = "#FF000066", cex=0.1)
+legend("topright", lty = rep(1, 4), bty = "n",
+       col = c("black", "#DF99FF99", "#72BF4099", "#FF000099"), 
+       legend = c("Kaplan-Meier curve", "Exponential", "Weibull", "Gamma"))
 
