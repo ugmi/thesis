a9604cb876f243b40301840125170eb6ebd02a7d
ugnemi <ugnemi@kth.se>
2026-02-03 11:04:25 +0100

    Created a plotting function for CIs, added CIs to existing plots, fitted a parametric Weibull model

--- data_generation_v3.R
+++ data_generation_v3.R
@@ -20,7 +20,7 @@ summary(t/365.25)
 
 o <- sort(t)
 plot(o, 1 - pweibull(o, 0.8, 500), xlab = "Days", ylab = "Survival probability",
-     main = "Weibull distribution with \nshape=0.8 and scale=500", pch=20)
+     main = "Weibull distribution with \nshape=0.8 and scale=500", pch = 20)
 
 # We assume the data generated above is the "true" distribution.
 # However, in real data, the measurements are discrete, often rounded to the day.
@@ -52,21 +52,8 @@ u <- ifelse(df.obs$status == "Dead", l + 1, Inf)
 fy <- function(l, u, b) Fz(u, b) - Fz(l, b)
 logL <- function(b, l, u) -sum(log(fy(l, u, b)))
 
-# Try different distributions
-Fz <- function(z, b) pexp(z, rate = b)
-mle.exp <- nlm(logL, 1/100, l, u, hessian = TRUE)
-
-Fz <- function(z, b) pweibull(z, shape = b[1], scale = b[2])
-mle.weibull <- nlm(logL, c(0.5, 400), l, u, hessian = TRUE)
-
-Fz <- function(z, b) plogis(z, location = b[1], scale = b[2])
-mle.logis <- nlm(logL, c(0, 400), l, u, hessian = TRUE)
-
-Fz <- function(z, b) pgamma(z, shape = b[1], rate = b[2])
-mle.gamma <- nlm(logL, c(1, 1/100), l, u, hessian = TRUE)
-
 # Confidence interval formula
-wilson.CI <- function(p.hat, n, alpha=0.05) {
+wilsonCI <- function(p.hat, n, alpha=0.05) {
   z <- qnorm(1 - alpha/2)
   lower <- (p.hat + z^2/(2*n))/(1 + z^2/n) - 
     z*sqrt(p.hat*(1 - p.hat)/n + z^2/(4*n^2))/(1 + z^2/n)
@@ -75,19 +62,48 @@ wilson.CI <- function(p.hat, n, alpha=0.05) {
   return(list("lower"=lower, "upper"=upper))
 }
 
+plotCI <- function(x, lower, upper, col, fill=NA) {
+  if (!is.na(fill)) {
+    polygon(c(rev(x), x), c(rev(lower), upper), col = fill, border = NA)
+  }
+  lines(x, lower, col = col, lty = "dashed")
+  lines(x, upper, col = col, lty = "dashed")
+}
+
+# Try different distributions
+Fz <- function(z, b) pexp(z, rate = b)
+mle.exp <- nlm(logL, 1/100, l, u, hessian = TRUE)
+CI.exp <- wilsonCI(1 - pexp(o, mle.exp$estimate), N)
+
+Fz <- function(z, b) pweibull(z, shape = b[1], scale = b[2])
+mle.weibull <- nlm(logL, c(0.5, 400), l, u, hessian = TRUE)
+CI.weibull <- wilsonCI(1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]), N)
+
+Fz <- function(z, b) plogis(z, location = b[1], scale = b[2])
+mle.logis <- nlm(logL, c(0, 400), l, u, hessian = TRUE)
+CI.logis <- wilsonCI(1 - plogis(o, mle.logis$estimate[1], mle.logis$estimate[2]), N)
+
+Fz <- function(z, b) pgamma(z, shape = b[1], rate = b[2])
+mle.gamma <- nlm(logL, c(1, 1/100), l, u, hessian = TRUE)
+CI.gamma <- wilsonCI(1 - pgamma(o, mle.gamma$estimate[1], mle.gamma$estimate[2]), N)
+
 # Plot and compare the distributions
-plot(o, 1 - pweibull(o, 0.8, 500), pch=20, main = "Fitted survival curves",
-     xlab = "Days", ylab = "Survival probability", cex=0.1)
-points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#DF99FF66", cex=0.1)
+plot(o, 1 - pweibull(o, 0.8, 500), pch = 20, main = "Fitted survival curves",
+     xlab = "Days", ylab = "Survival probability", cex = 0.05)
+points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#DF99FF66", cex = 0.1)
+plotCI(o, CI.exp[[1]], CI.exp[[2]], "#DF99FF33")
 points(o, 1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]), 
-       pch = 20, col = "#72BF4066", cex=0.1)
+       pch = 20, col = "#72BF4066", cex = 0.1)
+plotCI(o, CI.weibull[[1]], CI.weibull[[2]], "#72BF4033")
 points(o, 1 - plogis(o, mle.logis$estimate[1], mle.logis$estimate[2]), 
-       pch = 20, col = "#3060FF66", cex=0.1)
+       pch = 20, col = "#3060FF66", cex = 0.1)
+plotCI(o, CI.logis[[1]], CI.logis[[2]], "#3060FF33")
 points(o, 1 - pgamma(o, mle.gamma$estimate[1], mle.gamma$estimate[2]),
-       pch = 20, col = "#FF000066", cex=0.1)
-legend("topright", lty = rep(1, 5), bty = "n",
-       col = c("black", "#DF99FF99", "#72BF4099", "#3060FF99", "#FF000099"), 
-       legend = c("True distribution", "Exponential", "Weibull", "Logistic", "Gamma"))
+       pch = 20, col = "#FF000066", cex = 0.1)
+plotCI(o, CI.gamma[[1]], CI.gamma[[2]], "#FF000033")
+legend("topright", lty = c(rep(1, 5), 2), bty = "n",
+       col = c("black", "#DF99FF99", "#72BF4099", "#3060FF99", "#FF000099", "#00000033"), 
+       legend = c("True distribution", "Exponential", "Weibull", "Logistic", "Gamma", "CI bounds"))
 
 # We see in the plot that Weibull and Gamma distributions are close to the truth.
 
@@ -95,9 +111,11 @@ legend("topright", lty = rep(1, 5), bty = "n",
 plot(o, 1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]),
      pch = 20, col = "red",  cex=0.1, main = "Weibull survival curves",
      xlab = "Days", ylab = "Survival probability")
+plotCI(o, CI.weibull[[1]], CI.weibull[[2]], "#FF000044", fill = "#FF000022")
 points(o, 1 - pweibull(o, 0.8, 500), pch = 20, cex=0.1)
-legend("topright", lty = c(1, 1), bty = "n", col = c("black", "red"),
-       legend = c("True distribution", "Fitted distribution"))
+legend("topright", lty = c(1, 1, 2), bty = "n",
+       col = c("black", "red", "#FF000044"),
+       legend = c("True distribution", "Fitted distribution", "CI bounds"))
 
 # Bias (model parameters)
 c(0.8, 500) - mle.weibull$estimate  # -0.03495806 -39.29334380
@@ -111,6 +129,8 @@ c(0.8, 500) - mle.weibull$estimate  # -0.03495806 -39.29334380
 # Conventional approaches ------------------------------------------------------
 KM <- survfit(Surv(exit - entry, status == "Dead") ~ 1, data = df.obs, 
               conf.type = "log-log")
+CI.size.KM <- KM$upper - KM$lower
+v <- KM$time
 plot(KM, main = "Kaplan-Meier survival curve", xlab = "Days", ylab = "Survival",
      col = "red", xlim = c(0, 5000))
 points(o, 1 - pweibull(o, 0.8, 500), pch = 20, col = "black", cex = 0.1)
@@ -119,14 +139,17 @@ points(o, 1 - pweibull(o, 0.8, 500), pch = 20, col = "black", cex = 0.1)
 
 # Compare with some of the fitted models from before
 plot(KM, main = "Kaplan-Meier vs proposed approach", xlab = "Days", ylab = "Survival")
-points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#DF99FF66", cex = 0.1)
+points(o, 1 - pexp(o, mle.exp$estimate), pch = 20, col = "#DF99FF66", cex = 0.07)
+plotCI(o, CI.exp[[1]], CI.exp[[2]], "#DF99FF33")
 points(o, 1 - pweibull(o, mle.weibull$estimate[1], mle.weibull$estimate[2]), 
-       pch = 20, col = "#72BF4066", cex = 0.1)
+       pch = 20, col = "#72BF4066", cex = 0.07)
+plotCI(o, CI.weibull[[1]], CI.weibull[[2]], "#72BF4033")
 points(o, 1 - pgamma(o, mle.gamma$estimate[1], mle.gamma$estimate[2]),
-       pch = 20, col = "#FF000066", cex = 0.1)
+       pch = 20, col = "#FF000066", cex = 0.07)
+plotCI(o, CI.gamma[[1]], CI.gamma[[2]], "#FF000033")
 legend("topright", lty = rep(1, 4), bty = "n",
-       col = c("black", "#DF99FF99", "#72BF4099", "#FF000099"), 
-       legend = c("Kaplan-Meier curve", "Exponential", "Weibull", "Gamma"))
+       col = c("black", "#DF99FF99", "#72BF4099", "#FF000099", "#00000033"), 
+       legend = c("Kaplan-Meier curve", "Exponential", "Weibull", "Gamma", "CI bounds"))
 
 # Mean squared error
 mean((1 - pweibull(KM$time, 0.8, 500) - KM$surv)^2)
@@ -137,3 +160,21 @@ mean((pweibull(KM$time, 0.8, 500) - pexp(KM$time, mle.exp$estimate))^2)
 # Bias (1-year survival probability)
 (1 - pweibull(365.25, 0.8, 500)) - KM$surv[290] # since KM$time[290] = 365
 # -0.03037433
+
+# Parametric models ------------------------------------------------------------
+fit.weibull <- survreg(Surv(exit - entry, status == "Dead") ~ 1, 
+                       data = df.obs[df.obs$exit - df.obs$entry != 0,], 
+                       dist = "weibull")
+plot(KM$time, 1 - pweibull(KM$time, shape = 1/fit.weibull$scale, scale = exp(fit.weibull$coefficients)),
+     xlab = "Days", ylab = "Survival", pch = 20, cex = 0.1, col = "red")
+points(v, 1 - pweibull(v, mle.weibull$estimate[1], mle.weibull$estimate[2]), 
+       pch = 20, col = "#72BF4066", cex = 0.1)
+points(v, 1 - pweibull(v, 0.8, 500), pch = 20, cex = 0.1)
+
+# Bias (model parameters)
+c(0.8, 500) - as.numeric(c(1/fit.weibull$scale, exp(fit.weibull$coefficients)))  #  -0.07256524 -39.81796054
+
+# Likelihood formula
+
+
+
