917ac14ad0ec5e187f598aa9bd2fd8f77f5a8352
ugnemi <ugnemi@kth.se>
2026-02-04 10:14:40 +0100

    Added plots of the hazards, adjusted coarsening intervals

--- data_generation_v5.R
+++ data_generation_v5.R
@@ -15,10 +15,24 @@ Nb <- 400
 
 # Generate survival times
 ta <- rweibull(Na, shape = 0.8, scale = 500)
-tb <- rweibull(Nb, shape = 0.7, scale = 600)
+tb <- rweibull(Nb, shape = 1.5, scale = 800)
 
-plot(ta, 1 - pweibull(ta, 0.8, 500), col = "red", pch = 20, cex = 0.1)
-points(tb, 1 - pweibull(tb, 0.7, 600), col = "blue", pch = 20, cex = 0.1)
+# Plot the true survival distributions
+plot(ta, 1 - pweibull(ta, 0.8, 500), col = "red", pch = 20, cex = 0.1, 
+     main = "True survival distributions", xlab = "Days", ylab = "Survival")
+points(tb, 1 - pweibull(tb, 1.5, 800), col = "blue", pch = 20, cex = 0.1)
+legend("topright", lty = c(1, 1), bty = "n", col = c("red", "blue"),
+       legend = c("A", "B"))
+
+# Hazard function
+h <- function(y, b) dweibull(y, b[1], b[2])/(1 - pweibull(y, b[1], b[2]))
+
+# Plot the true hazard functions
+plot(ta, h(ta, c(0.8, 500)), col = "red", pch = 20, cex = 0.1,
+     main = "True hazard functions", xlab = "Days", ylab = "Hazard")
+points(tb, h(tb, c(1.5, 800)), col = "blue", pch = 20, cex = 0.1)
+legend("topright", lty = c(1, 1), bty = "n", col = c("red", "blue"),
+       legend = c("A", "B"))
 
 # Create the data set
 # We assume there are two possible treatments, A and B.
@@ -59,11 +73,13 @@ plotCI <- function(x, lower, upper, col, fill=NA) {
 }
 
 # Define coarsening limits
-l <- df.obs$exit - df.obs$entry
+l <- ifelse(df.obs$exit - df.obs$entry < 1, df.obs$exit - df.obs$entry, 
+            df.obs$exit - df.obs$entry - 1) 
 u <- ifelse(df.obs$status == "Dead", l + 1, Inf)
-
+# Covariate vector
 x <- df.obs$treatment
 
+# Proposed modelling distribution
 Fz <- function(z, b, x) {
   (x == "A")*pweibull(z, b[1], b[2]) + (x == "B")*pweibull(z, b[3], b[4])
 }
@@ -73,10 +89,15 @@ ob <- sort(tb)
 CI.A <- wilsonCI(1 - Fz(oa, mle.weibull$estimate, "A"), Na)
 CI.B <- wilsonCI(1 - Fz(ob, mle.weibull$estimate, "B"), Nb)
 
+# Hazard function
+hz <- function(z, b, x) {
+  ((x == "A")*dweibull(z, b[1], b[2]) + (x == "B")*dweibull(z, b[3], b[4])) / (1 - Fz(z, b, x))
+}
+
 # Plot the general curve
 o <- unique(c(oa, ob))
 
-# Plot the result by treatment
+# Plot estimated survival by treatment
 plot(ta, 1 - Fz(ta, mle.weibull$estimate, "A"), pch = 20, cex = 0.1, 
      col = "#FF000099", main = "Estimated survival \n(proposed approach)", 
      xlab = "Days", ylab = "Survival")
@@ -85,10 +106,23 @@ points(tb, 1 - Fz(tb, mle.weibull$estimate, "B"), pch = 20, cex = 0.1,
        col = "#0000FF99")
 plotCI(ob, CI.B[[1]], CI.B[[2]], "#0000FF44")
 points(ta, 1 - pweibull(ta, 0.8, 500), col = "#660000FF", pch = 20, cex = 0.1)
-points(tb, 1 - pweibull(tb, 0.7, 600), col = "#000066FF", pch = 20, cex = 0.1)
+points(tb, 1 - pweibull(tb, 1.5, 800), col = "#000066FF", pch = 20, cex = 0.1)
 legend("topright", lty = c(1, 1, 1, 1, 2), bty = "n", 
        col = c("#660000FF", "#FF000099", "#000066FF", "#0000FF99", "grey"),
        legend = c("True A", "Estimated A", "True B", "Estimated B", "CI bounds"))
+
+# Plot estimated hazard by treatment
+plot(ta, hz(ta, mle.weibull$estimate, "A"), pch = 20, cex = 0.1, 
+     col = "#FF000099", main = "Estimated hazard \n(proposed approach)", 
+     xlab = "Days", ylab = "Hazard")
+points(tb, hz(tb, mle.weibull$estimate, "B"), pch = 20, cex = 0.1, 
+       col = "#0000FF99")
+points(ta, h(ta, c(0.8, 500)), col = "#660000FF", pch = 20, cex = 0.1)
+points(tb, h(tb, c(1.5, 800)), col = "#000066FF", pch = 20, cex = 0.1)
+legend("topright", lty = c(1, 1, 1, 1), bty = "n", 
+       col = c("#660000FF", "#FF000099", "#000066FF", "#0000FF99"),
+       legend = c("True A", "Estimated A", "True B", "Estimated B"))
+
 
 # Conventional approaches ------------------------------------------------------
 KM <- survfit(Surv(exit - entry, status == "Dead") ~ treatment, data = df.obs, 
@@ -96,10 +130,10 @@ KM <- survfit(Surv(exit - entry, status == "Dead") ~ treatment, data = df.obs,
 
 plot(KM, col = c("#FF000099", "#0000FF99"), main = "Kaplan-Meier survival curves",
      xlab = "Days", ylab = "Survival")
-plotCI(KM$time[1:389], KM$lower[1:389], KM$upper[1:389], "#FF000044")
-plotCI(KM$time[390:685], KM$lower[390:685], KM$upper[390:685], "#0000FF44")
+plotCI(KM$time[1:385], KM$lower[1:385], KM$upper[1:385], "#FF000044")
+plotCI(KM$time[386:709], KM$lower[386:709], KM$upper[386:709], "#0000FF44")
 points(ta, 1 - pweibull(ta, 0.8, 500), col = "#990000FF", pch = 20, cex = 0.1)
-points(tb, 1 - pweibull(tb, 0.7, 600), col = "#000099FF", pch = 20, cex = 0.1)
+points(tb, 1 - pweibull(tb, 1.5, 800), col = "#000099FF", pch = 20, cex = 0.1)
 legend("topright", lty = c(1, 1, 1, 1, 2), bty = "n", 
        col = c("#660000FF", "#FF000099", "#000066FF", "#0000FF99", "grey"),
        legend = c("True A", "Estimated A", "True B", "Estimated B", "CI bounds"))
@@ -107,8 +141,8 @@ legend("topright", lty = c(1, 1, 1, 1, 2), bty = "n",
 # Compare
 plot(KM, col = c("#FF000099", "#0000FF99"), main = "Survival curves",
      xlab = "Days", ylab = "Survival", xlim=c(0,500))
-plotCI(KM$time[1:389], KM$lower[1:389], KM$upper[1:389], "#FF000044")
-plotCI(KM$time[390:685], KM$lower[390:685], KM$upper[390:685], "#0000FF44")
+plotCI(KM$time[1:385], KM$lower[1:385], KM$upper[1:385], "#FF000044")
+plotCI(KM$time[386:709], KM$lower[386:709], KM$upper[386:709], "#0000FF44")
 points(ta, 1 - Fz(ta, mle.weibull$estimate, "A"), pch = 20, cex = 0.1, 
        col = "#DD007788")
 plotCI(oa, CI.A[[1]], CI.A[[2]], "#FF007744")
